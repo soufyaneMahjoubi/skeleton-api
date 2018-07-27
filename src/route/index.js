ad1320046979c576e8113f25d50b9268cbf45786
soufyaneMahjoubi <soufyane.mahjoubi@epitech.eu>
2018-07-27 23:27:19 +0200

    add index route

--- src/route/index.js
+++ src/route/index.js
@@ -1,11 +1,5 @@
 const express = require('express');
 const router = express.Router();
-/* const routes = {
-    '/v1': './v1',
-};
-
-Object.keys(routes).forEach(key => router.use(key, require(routes[key]))); // eslint-disable-line */
-
 const example = require('./exemple/v1');
 
 router.use('/v1/exemple', example);
